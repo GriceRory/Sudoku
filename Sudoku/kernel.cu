a974b18c5002ba29a53666d63b627258119e10c2
Rory Grice <rory.grice01@gmail.com>
2020-12-13 21:57:48 +1300

    finished methods with checking that the board is valid

--- Sudoku/kernel.cu
+++ Sudoku/kernel.cu
@@ -1,11 +1,13 @@
 
+
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+//#include "cuNet.h"
 #include <stdio.h>
 
 struct board {
     int* contents;
+    int* guesses;
     int subBoardDimentions;
 };
 
@@ -13,21 +15,15 @@ void set_element(board b, int row, int col, int val);
 int get_element(board b, int row, int col);
 int validBoard(board b);
 int validSubBoard(board b, int sub);
+int validCol(board b, int col);
+int validCol(board b, int row);
 
-int main()
-{
+int main(){
 
     return 0;
 }
 
-//+-+-+-++-+-+-+-+-+
-//|5| | || | | | | |
-//+-+-+-++-+-+-+-+-+
-//|5| | || | | | | |
-//+-+-+-++-+-+-+-+-+
-//|5| | || | | | | |
-//+-+-+-++-+-+-+-+-+
-//+-+-+-++-+-+-+-+-+
+
 
 
 void set_element(board b, int row, int col, int val) {
@@ -65,6 +61,7 @@ int validSubBoard(board b, int sub) {
 int validCol(board b, int col) {
     int boardSize = b.subBoardDimentions * b.subBoardDimentions;
     for (int row = 0; row < boardSize; ++row) {
+        if (get_element(b, row, col) == 0) { continue; }
         for (int rowAhead = row + 1; rowAhead < boardSize; ++rowAhead) {
             if (get_element(b, row, col) == get_element(b, rowAhead, col)) { return 0; }
         }
@@ -74,6 +71,7 @@ int validCol(board b, int col) {
 int validRow(board b, int row) {
     int boardSize = b.subBoardDimentions * b.subBoardDimentions;
     for (int col = 0; col < boardSize; ++col) {
+        if (get_element(b, row, col) == 0) { continue; }
         for (int colAhead = col + 1; colAhead < boardSize; ++colAhead) {
             if (get_element(b, row, col) == get_element(b, row, colAhead)) { return 0; }
         }
@@ -84,17 +82,23 @@ int validRow(board b, int row) {
 
 int validBoard(board b) {
     //checking each subboard is valid
-    for (int subBoard = 0; subBoard < b.subBoardDimentions * b.subBoardDimentions; ++subBoard) {
+    int boardDim = b.subBoardDimentions * b.subBoardDimentions;
+
+    for (int subBoard = 0; subBoard < boardDim; ++subBoard) {
         if (!validSubBoard(b, subBoard)) {
             return 0;
         }
     }
+
     //checking each row is valid
-
-
+    for (int row = 0; row < boardDim; ++row) {
+        validRow(b, row);
+    }
 
     //checking each column is valid
-
+    for (int col = 0; col < boardDim; ++col) {
+        validRow(b, col);
+    }
 
 
     return 1;
